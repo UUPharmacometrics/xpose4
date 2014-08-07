1e0d828bf9fcd9e4df247ab55af2b421c95429a8
Andrew Hooker <andrew.c.hooker@gmail.com>
2014-08-07 11:10:35 +0200

    fix for R version 3.1.0

--- R/dOFV.vs.id.R
+++ R/dOFV.vs.id.R
@@ -23,70 +23,74 @@
 # directory under \share\licenses. If not, see http://www.gnu.org/licenses/.
 
 dOFV.vs.id <-
-    function(xpdb1,
-             xpdb2,
-             sig.drop=-3.84,
-             decrease.label.number=3,
-             increase.label.number=3,
-             id.lab.cex=0.6,
-             id.lab.pos=2,
-             type="o",
-             xlb="Number of subjects removed",
-             ylb=expression(paste(Delta,"OFV")),
-             main="Default",
-             sig.line.col = "red",
-             sig.line.lty = "dotted",
-             tot.line.col = "grey",
-             tot.line.lty = "dashed",
-             key=list(#title = expression(paste("Individual influence on ",Delta,"OFV")),
-                    columns = 1,
-                    lines = list(pch = c(super.sym$pch[1:2],NA,NA),
-                    type=list("o","o","l","l"),
-                    col = c(super.sym$col[1:2],sig.line.col,tot.line.col),
-                    lty = c(super.sym$lty[1:2],sig.line.lty,tot.line.lty)
-                    ),
-                    ## points = list(pch = c(super.sym$pch[1:2],NA),
-                    ## col = c(super.sym$col[1:2],"red")),
-                    text = list(c(
-                    expression(paste(Delta, OFV[i] < 0)),
-                    expression(paste(Delta, OFV[i] > 0)),
-                    expression(paste("Significant  ",Delta, OFV)),
-                    expression(paste("Total  ",Delta, OFV))
-                    ##"Individuals with OFV drop",
-                    ##"Individuals with OFV increase",
-                    ##"Significant drop in ")
-                    )),
-                    ##space="right",
-                    corner=c(0.95,0.5),border=T
+  function(xpdb1,
+           xpdb2,
+           sig.drop=-3.84,
+           decrease.label.number=3,
+           increase.label.number=3,
+           id.lab.cex=0.6,
+           id.lab.pos=2,
+           type="o",
+           xlb="Number of subjects removed",
+           ylb=expression(paste(Delta,"OFV")),
+           main="Default",
+           sig.line.col = "red",
+           sig.line.lty = "dotted",
+           tot.line.col = "grey",
+           tot.line.lty = "dashed",
+           key=list(#title = expression(paste("Individual influence on ",Delta,"OFV")),
+             columns = 1,
+             lines = list(pch = c(super.sym$pch[1:2],NA,NA),
+                          type=list("o","o","l","l"),
+                          col = c(super.sym$col[1:2],sig.line.col,tot.line.col),
+                          lty = c(super.sym$lty[1:2],sig.line.lty,tot.line.lty)
              ),
-             ...)
+             ## points = list(pch = c(super.sym$pch[1:2],NA),
+             ## col = c(super.sym$col[1:2],"red")),
+             text = list(c(
+               expression(paste(Delta, OFV[i] < 0)),
+               expression(paste(Delta, OFV[i] > 0)),
+               expression(paste("Significant  ",Delta, OFV)),
+               expression(paste("Total  ",Delta, OFV))
+               ##"Individuals with OFV drop",
+               ##"Individuals with OFV increase",
+               ##"Significant drop in ")
+             )),
+             ##space="right",
+             corner=c(0.95,0.5),border=T
+           ),
+           ...)
 {
-
-
-  if(is.null(xpdb2)){
-    cat("Comparison database needed for this plot!")
-    return(NULL)
-  }
-  
+    
+    
+    if(is.null(xpdb2)){
+      cat("Comparison database needed for this plot!")
+      return(NULL)
+    }
+    
     #require(graphics)
-
+    
     iv1 <- xpdb1@Data.firstonly
     ##str(iv2)
-
+    
     iv2 <- xpdb2@Data.firstonly
     ##str(iv2)
-
+    
     if(!all(iv1$ID == iv2$ID)){
-        cat("All ID labels for both databases must match\n")
-        return(NULL)
+      cat("All ID labels for both databases must match\n")
+      return(NULL)
     }
-
+    
     comp.frame <- data.frame(id=iv1$ID,obj1=iv1$OBJ,obj2=iv2$OBJ)
+    
+    # fix for R version 3.1.0
+    if(is.factor(comp.frame$obj1)) comp.frame$obj1 <- as.numeric(levels(comp.frame$obj1))[comp.frame$obj1] 
+    if(is.factor(comp.frame$obj2)) comp.frame$obj2 <- as.numeric(levels(comp.frame$obj2))[comp.frame$obj2] 
     comp.frame$d.obj <- comp.frame$obj2 - comp.frame$obj1
     ##str(comp.frame)
-
+    
     ##hist(comp.frame$d.obj)
-
+    
     ofv1 <- sum(comp.frame$obj1)
     ofv2 <- sum(comp.frame$obj2)
     d.ofv <- sum(comp.frame$d.obj)
@@ -94,64 +98,64 @@ dOFV.vs.id <-
     ##ofv2
     ##d.ofv
     ##ofv2-ofv1
-
-
+    
+    
     comp.frame$rnk <- rank(comp.frame$d.obj,ties.method="random")
     ##comp.frame[comp.frame$rnk==74,]
     ##comp.frame[comp.frame$rnk==1,]
-
+    
     max.rank <- max(comp.frame$rnk)
     id.increase <- c(NA)
     id.decrease<- c(NA)
     increase.sum <- c(d.ofv)
     decrease.sum <- c(d.ofv)
-
+    
     for(i in 1:max.rank){
-        ##i=1
-
-        ## compute largest decrease
-        tmp <- comp.frame[comp.frame$rnk==i,]
-        if(tmp$d.obj<=0){
-            id.decrease <- c(id.decrease,tmp$id)
-            decrease.sum <- c(decrease.sum,sum(comp.frame[comp.frame$rnk > i,"d.obj"]))
-        }
-
-        ## compute largest increase
-        tmp <- comp.frame[comp.frame$rnk==(max.rank-i+1),]
-        if(tmp$d.obj>0){
-            id.increase <- c(id.increase,tmp$id)
-            increase.sum <- c(increase.sum,
-                              sum(comp.frame[comp.frame$rnk < (max.rank - i +1),"d.obj"]))
-        }
+      ##i=1
+      
+      ## compute largest decrease
+      tmp <- comp.frame[comp.frame$rnk==i,]
+      if(tmp$d.obj<=0){
+        id.decrease <- c(id.decrease,tmp$id)
+        decrease.sum <- c(decrease.sum,sum(comp.frame[comp.frame$rnk > i,"d.obj"]))
+      }
+      
+      ## compute largest increase
+      tmp <- comp.frame[comp.frame$rnk==(max.rank-i+1),]
+      if(tmp$d.obj>0){
+        id.increase <- c(id.increase,tmp$id)
+        increase.sum <- c(increase.sum,
+                          sum(comp.frame[comp.frame$rnk < (max.rank - i +1),"d.obj"]))
+      }
     }
-
+    
     ##length(id.increase)
     ##length(id.decrease)
-
+    
     n.removed <- c(1:max(length(id.increase),length(id.decrease)))-1
     ##n.removed
-
+    
     if(length(id.increase) < length(n.removed)){
-        n.na <- length(n.removed) - length(id.increase)
-        id.increase <- c(id.increase,rep(NA,times=n.na))
-        increase.sum <- c(increase.sum,rep(NA,times=n.na))
+      n.na <- length(n.removed) - length(id.increase)
+      id.increase <- c(id.increase,rep(NA,times=n.na))
+      increase.sum <- c(increase.sum,rep(NA,times=n.na))
     }
     if(length(id.decrease) < length(n.removed)){
-        n.na <- length(n.removed) - length(id.decrease)
-        id.decrease <- c(id.decrease,rep(NA,times=n.na))
-        decrease.sum <- c(decrease.sum,rep(NA,times=n.na))
+      n.na <- length(n.removed) - length(id.decrease)
+      id.decrease <- c(id.decrease,rep(NA,times=n.na))
+      decrease.sum <- c(decrease.sum,rep(NA,times=n.na))
     }
-
+    
     plot.frame <- data.frame(n.removed,
                              id.increase,
                              increase.sum,
                              id.decrease,
                              decrease.sum)
-
-
-
+    
+    
+    
     ##plot.frame
-
+    
     default.plot.title <- paste("Individual influence on change in OFV\n",
                                 "(Run",xpdb2@Runno," - Run",xpdb1@Runno,")",sep="" )
     plotTitle <- xpose.multiple.plot.title(object=xpdb1,
@@ -159,11 +163,11 @@ dOFV.vs.id <-
                                            main=main,
                                            no.runno=T,
                                            ...)
-
+    
     ##id label
     idlab.decrease <- subset(plot.frame,n.removed > 0 & n.removed <= decrease.label.number)
     idlab.increase <- subset(plot.frame,n.removed > 0 & n.removed <= increase.label.number)
-
+    
     super.sym <- trellis.par.get("superpose.symbol")
     xplot <- xyplot(decrease.sum+increase.sum~n.removed,data=plot.frame,
                     type=type,
@@ -176,30 +180,30 @@ dOFV.vs.id <-
                     id.lab.cex = id.lab.cex,
                     id.lab.pos = id.lab.pos,
                     panel=function(x,y,
-                    sig.line.col,sig.line.lty,
-                    tot.line.col,tot.line.lty,
-                    id.lab.cex,id.lab.pos,
-                    ...){
-                        panel.xyplot(x,y,...)
-                        panel.abline(h=sig.drop,col=sig.line.col,lty=sig.line.lty,...)
-                        panel.abline(h=y[1],col=tot.line.col,lty=tot.line.lty,...)
-                        ltext(x=idlab.decrease$n.removed,y=idlab.decrease$decrease.sum,
-                              label=idlab.decrease$id.decrease,
-                              cex=id.lab.cex,
-                              pos=id.lab.pos,
-                                        #adj=c(2,1),
-                              ...)
-                        ltext(x=idlab.increase$n.removed,y=idlab.increase$increase.sum,
-                              label=idlab.increase$id.increase,
-                              cex=id.lab.cex,
-                              pos=id.lab.pos,
-                                        #adj=c(2,1),
-                              ...)
+                                   sig.line.col,sig.line.lty,
+                                   tot.line.col,tot.line.lty,
+                                   id.lab.cex,id.lab.pos,
+                                   ...){
+                      panel.xyplot(x,y,...)
+                      panel.abline(h=sig.drop,col=sig.line.col,lty=sig.line.lty,...)
+                      panel.abline(h=y[1],col=tot.line.col,lty=tot.line.lty,...)
+                      ltext(x=idlab.decrease$n.removed,y=idlab.decrease$decrease.sum,
+                            label=idlab.decrease$id.decrease,
+                            cex=id.lab.cex,
+                            pos=id.lab.pos,
+                            #adj=c(2,1),
+                            ...)
+                      ltext(x=idlab.increase$n.removed,y=idlab.increase$increase.sum,
+                            label=idlab.increase$id.increase,
+                            cex=id.lab.cex,
+                            pos=id.lab.pos,
+                            #adj=c(2,1),
+                            ...)
                     },
                     ##auto.key =T
                     main= plotTitle,
                     key = key,
                     ...
-                    )
+    )
     return(xplot)
-}
+  }
