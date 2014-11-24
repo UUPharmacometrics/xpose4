4b881ae4f2ac5a7f995e1975798f29dc88a8a44e
Andrew Hooker <andrew.c.hooker@gmail.com>
2014-11-24 08:16:48 +0100

    getting ready for release

---
+++ tools/check_xpose.sh
@@ -0,0 +1,9 @@
+#R  CMD build ..
+
+# Potential R CMD check commands
+# R CMD check --as-cran C:\Users\Andy\Documents\Xpose\xpose4_CVS\xpose4\xpose4data
+# R CMD check C:\Users\Andy\Documents\Xpose\xpose4_CVS\xpose4\xpose4data
+# R CMD check --as-cran xpose4data_4.4.3.tar.gz
+# R CMD check xpose4data_4.4.3.tar.gz
+
+R CMD check --as-cran 
---
+++ tools/set_path.bat
@@ -0,0 +1,2 @@
+SET PATH=C:\Program Files\R\R-3.0.2\bin\x64;%PATH%
+SET PATH=c:\Rtools\bin;c:\Rtools\gcc-4.6.3\bin;%PATH%
