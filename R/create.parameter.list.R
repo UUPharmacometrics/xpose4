617015455d6a748cd9b167044105ec99d42d233f
Andrew Hooker <andrew.c.hooker@gmail.com>
2015-04-16 14:43:49 +0200

    update for off diagnonal sigma

--- R/create.parameter.list.R
+++ R/create.parameter.list.R
@@ -35,12 +35,12 @@
     cat("The output file couldn't be found in the current directory.\n")
     return()
   }
-
+  
   ## Count the number of parameters
   npar.list   <- calc.npar(x)
-
+  
   #attach(npar.list, warn.conflicts=F)
-
+  
   npar <- npar.list$npar
   nth <- npar.list$nth
   nseth <- npar.list$nseth
@@ -48,13 +48,13 @@
   nseom <- npar.list$nseom
   nsi <- npar.list$nsi
   nsesi <- npar.list$nsesi
-
-
-
+  
+  
+  
   seenterm <- seenobj <- seenth <- seenom <- seensi <- seenseth <- seenseom <-
     seensesi <- seennth <- seennom <- 0
   #attach(x, warn.conflicts=F)
-
+  
   term <- x$term
   ofv <- x$ofv
   thetas <- x$thetas
@@ -63,7 +63,7 @@
   sethetas <- x$sethetas
   seomegas <- x$seomegas
   sesigmas <- x$sesigmas
-
+  
   
   if(!any(is.null(term)))
     seenterm <- 1
@@ -81,22 +81,22 @@
     seenseom <- 1
   if(!any(is.null(sesigmas)) && nsesi !=0)
     seensesi <-1
-
+  
   ## Add parameters
-
+  
   ## Construct the parameter information that is to be added to the screen
   ## This will be in the form of 1 or two arrays, the first being the array
   ## with parameter estimates and the second with SEs
   ## The nonparametrics are added to the parameter array afterwards
   ## The SE array will be constructed wether or not there are SE but filled
   ## with 0.
-
+  
   ## If we haven't got any parameters in the lst-file
   if(seenth != 1) {
     cat("No parameters found, check your NONMEM output file.\n")
     return()
   }
-
+  
   ## If we have thetas (we should have them!)
   if(seenth == 1) {
     nth           <- length(thetas)
@@ -105,14 +105,14 @@
     parnam[1:nth] <- paste("TH",1:nth,sep="")
     parval[1:nth] <- format.default(thetas,digits=2)
   }
-
+  
   ## If we have SE for the thetas
   if(seenseth == 1) {
     nseth           <- length(sethetas)
     separnam        <- 0
     separval        <- 0
     separnam[1:nseth] <- paste("RSE TH",1:nseth,sep="")
-
+    
     ## To avoid division by zero or NA
     selzero           <- thetas == 0
     selna             <- is.na(sethetas)
@@ -129,7 +129,7 @@
     separnam[1:nth] <- paste("RSE TH",1:nth,sep="")
     separval[1:nth] <- 0
   }
-
+  
   ## If we have omegas
   if(seenom == 1 ) {
     nomega <- length(omega)
@@ -138,55 +138,55 @@
       if(nomega == 1) sel <- T
       if(i == 1) sel <- T
       if(length(omega[[i]][sel]) == 1 || i == 1) {        # Must be a diagonal omega
-
-	parnam[length(parnam)+1]  <- paste("OM",i,":",i,sep="")
-
+        
+        parnam[length(parnam)+1]  <- paste("OM",i,":",i,sep="")
+        
         ## If the first omega is fixed to 0
-	if(omega[[i]][sel] == 0) {
+        if(omega[[i]][sel] == 0) {
           parval[length(parval)+1]  <-
             format.default(0,digits=2)
           ##separnam[length(separnam)+1]  <- paste("CT OM",i,":",i,sep="")
           separval[length(separval)+1]  <- ""
         } else {
-
+          
           parval[length(parval)+1]  <-
             format.default(sqrt(omega[[i]][sel]),digits=2)
           separnam[length(separnam)+1]  <- paste("CT OM",i,":",i,sep="")
           separval[length(separval)+1]  <- ""
         }
-
-
+        
+        
       } else { # There are off-diagonals or the whole row is zero
-
-	## The diagonal element
-	parnam[length(parnam)+1]  <- paste("OM",i,":",i,sep="")
-	parval[length(parval)+1]  <-
-	  format.default(sqrt(omega[[i]][i]),digits=2)
-
-	separnam[length(separnam)+1]  <- paste("CT OM",i,":",i,sep="")
-	separval[length(separval)+1]  <- ""
-
+        
+        ## The diagonal element
+        parnam[length(parnam)+1]  <- paste("OM",i,":",i,sep="")
+        parval[length(parval)+1]  <-
+          format.default(sqrt(omega[[i]][i]),digits=2)
+        
+        separnam[length(separnam)+1]  <- paste("CT OM",i,":",i,sep="")
+        separval[length(separval)+1]  <- ""
+        
         ## Loop over the off-diagonals
-	for(j in 1:(length(omega[[i]])-1)) {
-
-	  if(omega[[i]][j] == 0) next
-	  if(omega[[i]][j] != 0) {
-	    parnam[length(parnam)+1]  <- paste("OM",i,":",j,sep="")
-	    parval[length(parval)+1]  <-
-	      format.default(omega[[i]][j]/sqrt(omega[[i]][i]*omega[[j]][j]),digits=2)
-
-	    separnam[length(separnam)+1]  <- paste("CT OM",i,":",j,sep="")
-	    separval[length(separval)+1]  <- ""
-	  }
-	}
+        for(j in 1:(length(omega[[i]])-1)) {
+          
+          if(omega[[i]][j] == 0) next
+          if(omega[[i]][j] != 0) {
+            parnam[length(parnam)+1]  <- paste("OM",i,":",j,sep="")
+            parval[length(parval)+1]  <-
+              format.default(omega[[i]][j]/sqrt(omega[[i]][i]*omega[[j]][j]),digits=2)
+            
+            separnam[length(separnam)+1]  <- paste("CT OM",i,":",j,sep="")
+            separval[length(separval)+1]  <- ""
+          }
+        }
       }
     }
   }
-
+  
   ## If we have SE for the omegas -- fill in their values
   if(seenseom == 1) {
     n <- length(thetas)           # n - is a flag that makes the values appear
-                                  #     in the right place
+    #     in the right place
     nseomega <- length(seomegas)
     for(i in 1:nseomega) {
       ## Select the non-zero omegas
@@ -194,20 +194,20 @@
       
       if(nseomega == 1) sel <- T
       if(i == 1) sel <- T
-
+      
       if(length(seomegas[[i]][sel]) == 1) {
         ## Must be a diagonal omega
-
+        
         n <- n+1
         separnam[n] <-
           paste("RSE OM",i,":",i,sep="")
-
+        
         if(omega[[i]][sel] == 0) { ## If first omega is fixed to 0
           separval[n]  <- ""
         } else {
           if(is.na(seomegas[[i]][sel])){
             separval[n]  <- ""
-          } else {
+          } else {  
             if(seomegas[[i]][sel] == 0){
               separval[n]  <- ""
             } else {
@@ -216,11 +216,11 @@
             }
           }
         }
-
+        
       } else {
         ## There are off-diagonals
         n <- n+1
-	separnam[n]  <- paste("RSE OM",i,":",i,sep="")
+        separnam[n]  <- paste("RSE OM",i,":",i,sep="")
         if(is.na(seomegas[[i]][i])){
           separval[n]  <- ""
         } else {
@@ -231,14 +231,14 @@
               format.default(seomegas[[i]][i]/abs(omega[[i]][i]),digits=2)
           }
         }
-
+        
         ## Loop over the off-diagonals
-	for(j in 1:(length(seomegas[[i]])-1)) {
-	  if(omega[[i]][j] == 0) next
-	  if(omega[[i]][j] != 0) {
-	    n <- n +1
-	    separnam[n]  <-
-	      paste("RSE OM",i,":",j,sep="")
+        for(j in 1:(length(seomegas[[i]])-1)) {
+          if(omega[[i]][j] == 0) next
+          if(omega[[i]][j] != 0) {
+            n <- n +1
+            separnam[n]  <-
+              paste("RSE OM",i,":",j,sep="")
             if(is.na(seomegas[[i]][j])){
               separval[n]  <- ""
             } else {
@@ -249,66 +249,64 @@
                   format.default(seomegas[[i]][j]/abs(omega[[i]][j]),digits=2)
               }
             }
-	  }
-	}
+          }
+        }
       }
     }
   }
   
   ## capture the length of the parameter vector before sigma added
   th.om.par.length <- length(parnam)
-
+  
   ## If we have sigmas
   if(seensi == 1) {
-      nsigma <- length(sigma)
-      for(i in 1:nsigma) {
+    nsigma <- length(sigma)
+    for(i in 1:nsigma) {
       sel <- sigma[[i]] != 0
-
+      
       if(length(sigma[[i]][sel]) == 1) {        # Must be a diagonal omega
-
-	parnam[length(parnam)+1]  <- paste("SI",i,":",i,sep="")
-	parval[length(parval)+1]  <-
-	  format.default(sqrt(sigma[[i]][sel]),digits=2)
-
-	separnam[length(separnam)+1]  <- paste("RSE SI",i,":",i,sep="")
-	separval[length(separval)+1]  <- ""
-
+        
+        parnam[length(parnam)+1]  <- paste("SI",i,":",i,sep="")
+        parval[length(parval)+1]  <-
+          format.default(sqrt(sigma[[i]][sel]),digits=2)
+        
+        separnam[length(separnam)+1]  <- paste("RSE SI",i,":",i,sep="")
+        separval[length(separval)+1]  <- ""
+        
       } else {                                  # There are off-diagonals
-
-	## The diagonal element
-	parnam[length(parnam)+1]  <- paste("SI",i,":",i,sep="")
-	parval[length(parval)+1]  <-
-	  format.default(sqrt(sigma[[i]][i]),digits=2)
-
-	separnam[length(separnam)+1]  <- paste("RSE SI",i,":",i,sep="")
-	separval[length(separval)+1]  <- ""
-
+        ## The diagonal element
+        parnam[length(parnam)+1]  <- paste("SI",i,":",i,sep="")
+        parval[length(parval)+1]  <-
+          format.default(sqrt(sigma[[i]][i]),digits=2)
+        
+        separnam[length(separnam)+1]  <- paste("RSE SI",i,":",i,sep="")
+        separval[length(separval)+1]  <- ""
+        
         ## Loop over the off-diagonals
-	for(j in 1:(length(sigma[[i]])-1)) {
+        for(j in 1:(length(sigma[[i]])-1)) {
           if(sigma[[i]][j] == 0) next
-	  if(sigma[[i]][j] != 0) {
-	    parnam[length(parnam)+1]  <- paste("SI",i,":",j,sep="")
-	    parval[length(parval)+1]  <-
-	      format.default(sqrt(sigma[[i]][j]),digits=2)
-
-	    separnam[length(separnam)+1]  <- paste("RSE SI",i,":",j,sep="")
-	    separval[length(separval)+1]  <- ""
-	  }
-	}
+          if(sigma[[i]][j] != 0) {
+            parnam[length(parnam)+1]  <- paste("SI",i,":",j,sep="")
+            parval[length(parval)+1]  <-
+              format.default(sigma[[i]][j]/sqrt(sigma[[i]][i]*sigma[[j]][j]),digits=2)
+            separnam[length(separnam)+1]  <- paste("RSE SI",i,":",j,sep="")
+            separval[length(separval)+1]  <- ""
+          }
+        }
       }
     }
   }
-
+  
   ## If we have SE for the sigmas -- fill in their values
   if(seensesi == 1) {
     n <- th.om.par.length
-
+    
     nsesigma <- length(sesigmas)
     for(i in 1:nsesigma) {
       sel <- sigma[[i]] != 0
-
+      
       if(nsesigma == 1) sel <- T
-
+      
       if(length(sesigmas[[i]][sel]) == 1) {
         ## Must be a diagonal omega
         n <- n+1
@@ -332,10 +330,10 @@
         ## There are off-diagonals
         n <- n+1
         separnam[n]<- paste("RSE SI",i,":",i,sep="")
-        if(is.na(sesigmas[[i]][sel])){
+        if(is.na(sesigmas[[i]][i])){
           separval[n]  <- ""
         } else {
-          if(seomegas[[i]][sel] == 0) {
+          if(sesigmas[[i]][i] == 0) {
             separval[n]  <- ""
           } else {
             separval[n]<-
@@ -343,13 +341,13 @@
           }
         }
         ## Loop over the off-diagonals
-	for(j in 1:(length(sesigmas[[i]])-1)) {
-
-	  if(sigma[[i]][j] == 0) next
-	  if(sigma[[i]][j] != 0) {
-	    n <- n+1
-	    separnam[n] <-
-	      paste("RSE SI",i,":",j,sep="")
+        for(j in 1:(length(sesigmas[[i]])-1)) {
+          
+          if(sigma[[i]][j] == 0) next
+          if(sigma[[i]][j] != 0) {
+            n <- n+1
+            separnam[n] <-
+              paste("RSE SI",i,":",j,sep="")
             if(is.na(sesigmas[[i]][j])){
               separval[n]  <- ""
             } else {
@@ -360,13 +358,13 @@
                   format.default(sesigmas[[i]][j]/abs(sigma[[i]][j]),digits=2)
               }
             }
-	  }
-	}
+          }
+        }
       }
     }
   }
-
-
+  
+  
   ret.list <- list(term = term, ofv = ofv,
                    seenterm = seenterm,
                    seenobj = seenobj,
@@ -381,13 +379,13 @@
                    parval = parval,
                    separnam = separnam,
                    separval = separval
-                   )
-
-
-
+  )
+  
+  
+  
   #detach(x)
   #detach(npar.list)
-
+  
   return(ret.list)
-
+  
 }
