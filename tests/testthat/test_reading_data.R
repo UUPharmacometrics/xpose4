f0c00d0b616a427d80a2662574c34917630f895f
Andrew Hooker <andrew.c.hooker@gmail.com>
2016-02-24 14:59:24 +0100

    updated tests

--- tests/testthat/test_reading_data.R
+++ tests/testthat/test_reading_data.R
@@ -14,26 +14,34 @@ test_that("readr methods work",{
    
   setwd("/Users/ahooker/Documents/_PROJECTS/Xpose/Examples/other_examples/Andy_Simpraz")
   
+  sink("tmp.txt")
   xpdb <- xpose.data(5)
   xpdb_1 <- xpose.data(5,method="slow") 
   #xpdb_2 <- xpose.data(5,method="readr_2") 
   #xpdb_3 <- xpose.data(5,method="readr_3") 
+  sink()
+  file.remove("tmp.txt")
   
- 
   compare_xpdb(xpdb,xpdb_1)
   #compare_xpdb(xpdb,xpdb_2)
   #compare_xpdb(xpdb,xpdb_3)
   
   setwd("/Users/ahooker/Documents/_PROJECTS/Xpose/Examples/other_examples/final_954")
   
+  sink("tmp.txt")
   xpdb <- xpose.data(954)
   xpdb_1 <- xpose.data(954,method="slow") 
+  sink()
+  file.remove("tmp.txt")
   
   compare_xpdb(xpdb,xpdb_1)
   
   setwd("/Users/ahooker/Documents/_PROJECTS/Xpose/Examples/other_examples/hands_on_2_solutions")
+  sink("tmp.txt")
   xpdb <- xpose.data(12)
   xpdb_1 <- xpose.data(12,method="slow") 
+  sink()
+  file.remove("tmp.txt")
   
   compare_xpdb(xpdb,xpdb_1)
   
